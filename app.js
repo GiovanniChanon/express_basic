693170d8039576386c2aae2d2e2c1359abdc34d3
GiovanniChanon <giochanon97@gmail.com>
2022-04-29 16:32:43 -0500

    Agregando ruta con query params

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { get } = require('express/lib/response')
 const app = express()
 const port = 3000
 
@@ -17,4 +18,8 @@ app.get('/launchx', (req, res) => {
 app.get('/explorersInNode', (req, res) => {
     const explorer = {name: "explorer", msg: "Hello"}
     res.send(explorer)
+})
+
+app.get('/explorers/:explorerName', (req, res) => {
+    res.send(req.params)
 })
